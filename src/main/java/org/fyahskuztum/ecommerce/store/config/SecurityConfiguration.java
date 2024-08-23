daa01aa03864ac835e3c9029cf8ed2ace3476f71
Fyah757 <ponds.stacie@outlook.com>
2024-08-23 10:51:46 -0400

    rebuild

---
+++ src/main/java/org/fyahskuztum/ecommerce/store/config/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package org.fyahskuztum.ecommerce.store.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails wilbur = User.builder()
+                .username("wilbur")
+                .password("{noop}123")
+                .roles("SUPERUSER")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(wilbur, mary, susan);
+    }
+}
---
+++ src/main/java/org/fyahskuztum/ecommerce/store/controller/YourController.java
@@ -0,0 +1,16 @@
+package org.fyahskuztum.ecommerce.store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class YourController {
+
+    @GetMapping("/")
+    public String showHome() {
+
+        return "home";
+    }
+}
+
+
